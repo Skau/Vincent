7232e5b6d91b56db93ccbf2084e042e6add1ab78
Espen <espenhg@hotmail.com>
2018-05-07 13:40:06 +0200

    Did some stuff
    - fixed movingplatform bug
    - platforms

--- Source/MappeOppgave/WorldActors/MovingPlatform.cpp
+++ Source/MappeOppgave/WorldActors/MovingPlatform.cpp
@@ -99,7 +99,8 @@ void AMovingPlatform::SwapDirection()
 	GlobalTargetLocation = Swap;
 
 	TotalDistanceToTravel = FVector::DotProduct((GlobalTargetLocation - GetActorLocation()), (GlobalTargetLocation - GlobalStartLocation).GetSafeNormal());
-	UE_LOG(LogTemp, Warning, TEXT("Timer Ended"))
+
+	bTimerSet = false;
 }
 
 void AMovingPlatform::OnBeginOverlap(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
